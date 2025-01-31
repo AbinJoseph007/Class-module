302dcf3dab3b1c456561e86827861d9837f85712
AbinJoseph007 <abinjoseph12303@gmail.com>
2025-01-31 23:33:16 +0530

    again timestamp changed

--- index.js
+++ index.js
@@ -1236,9 +1236,7 @@ app.post('/submit-class', async (req, res) => {
 
 
 app.post('/register-class', async (req, res) => {
-  const { memberid, ...fields } = req.body;
-  const timestampField = Math.floor(Date.now() / 1000);
-
+  const { memberid, timestampField, ...fields } = req.body;
 
   try {
     // Validate Member ID (ensure it exists in the "Members" table)
